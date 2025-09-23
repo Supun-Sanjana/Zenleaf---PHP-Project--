d20c3c6136591a20aa82e565bac094a4718d537e
Supun Sanjana <infor.ssupun@gmail.com>
2025-09-23 22:49:04 +0530

    add br submit form

--- src/templates/supplier/header.php
+++ src/templates/supplier/header.php
@@ -1,7 +1,7 @@
 <header class="bg-gray-800 p-4 shadow-lg flex justify-between items-center">
     <div class="flex items-center space-x-4">
         <h1 class="text-2xl font-bold text-emerald-400 mr-20">ZenLeaf</h1>
-         <a href="./suppliers.php" class="text-gray-300 hover:text-white px-3 py-2 rounded-md transition-colors bg-emerald-600">Become a
+         <a href="./br_submit.php" class="text-gray-300 hover:text-white px-3 py-2 rounded-md transition-colors bg-emerald-600">Become a
         Supplier</a>
     </div>
 
